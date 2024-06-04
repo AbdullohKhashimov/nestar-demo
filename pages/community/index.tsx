e29bfb6fc0d5bda20dc102b8c4999c876e15f033
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-06-04 15:21:05 +0900

    feat: integration Header and Footer via Layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,5 +1,18 @@
-const CommunityList = () => {
-  return <div>Community List</div>;
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
+import { useState } from "react";
+
+const CommunityList: NextPage = () => {
+  //console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+  const [title, setTitle] = useState<string>("hello");
+  return (
+    <div>
+      COMMUNITY
+      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+        Press Me
+      </button>
+    </div>
+  );
 };
 
-export default CommunityList;
+export default withLayoutBasic(CommunityList);
