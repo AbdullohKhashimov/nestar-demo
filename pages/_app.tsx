6c95dd8f634b5a200d8c98130917b06985e63678
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-06-13 16:15:12 +0900

    feat: integration of apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import type { AppProps } from "next/app";
 import { useState } from "react";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
@@ -12,10 +14,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   // Socket.io, Redux, Mui
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,28 @@ import TopAgents from "@/libs/components/homepage/TopAgents";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
+  const {
+    loading: getPRopertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+  console.log("getPRopertiesData:", getPropertiesData);
   return (
     <Stack className={"home-page"}>
       <TrendProperties />
